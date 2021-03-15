b4951a1f35423ca66686e98a47fa46888d3d10eb
Ryan Morgan <nerdmomsrock@gmail.com>
2021-03-15 17:41:20 -0400

    basic css done

--- user-directory/src/App.js
+++ user-directory/src/App.js
@@ -1,29 +1,51 @@
-import React, {Component} from './react';
+import React, { Component } from "react";
 
-import './index.css';
+import "./index.css";
 
 class App extends Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
-      name: '',
-     location: '',
-     employer: '',
-     movies: [],
-
-
-    }
+      name: "",
+      job: "",
+      location: "",
+      employer: "",
+      movies: [],
+      id: null,
+    };
   }
-  render () {
+
+  render() {
     return (
-      <div className='App'>
-        <button className='Previous'>Previous</button>
-        <button id='middle'>Edit</button>
-        <button id='middle'>Delete</button>
-        <button id='middle'>New</button>
-        <button className='Next'>Next</button>
+      <div className="App">
+        <div className="header">
+          <div className="home">
+            <h2>Home</h2>
+          </div>
+        </div>
+        <div className="card">
+          <div className="top">
+            <div className="name">
+              <h1>{this.state.name}</h1>
+            </div>
+            <div className="number">
+              <h1>{this.state.id}</h1>
+            </div>
+          </div>
+          <div className="demographics"></div>
+          <div className="movies"></div>
+        </div>
+        <div className="buttons">
+          <button className="Previous">Previous</button>
+          <div className="middlewrapper">
+            <button id="middle">Edit</button>
+            <button id="middle">Delete</button>
+            <button id="middle">New</button>
+          </div>
+          <button className="Next">Next</button>
+        </div>
       </div>
-    )
+    );
   }
 }
 
